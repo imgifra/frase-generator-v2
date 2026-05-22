a4daa53ed22d2ef43d9438a5e8784f5010841462
imgifra <francoperezclaro@gmail.com>
2026-05-22 00:13:47 -0500

    wertwert

--- scripts/core/status.js
+++ scripts/core/status.js
@@ -25,11 +25,24 @@ const LOCK_STATUS = {
 const MAX_INTENTOS = 3;
 
 const BG_SEQUENCE = [
-  "#f4c400", // retroYellow
-  "#3d5afe", // retroBlue
-  "#e53935", // retroRed
-  "#f6f1e8", // retroWhite
-  "#0d0f14"  // retroBlack
+  // Ciclo original
+  "#f6f1e8", // crema
+  "#0d0f14", // negro
+  "#f4c400", // amarillo
+  "#3d5afe", // azul
+  "#e53935", // rojo
+
+  // Ciclo oscuro / melancólico
+  "#0d0208", // negro vino
+  "#1a0033", // morado noche
+  "#0a1628", // azul marino
+  "#1c0a00", // café oscuro
+
+  // Ciclo cálido / irónico
+  "#ff4d00", // naranja fuego
+  "#d4006a", // rosa fuerte
+  "#8d6e00", // mostaza
+  "#2e7d32"  // verde selva
 ];
 
 module.exports = {
